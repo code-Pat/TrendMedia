42b3f86fca50bdabc2a184d040fdce5e365dfbca
Donggeun Lee <patlee997@gmail.com>
2021-10-20 15:35:45 +0900

    Storyboard setup & connected to VCs

---
+++ TrendMedia/CastViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CastViewController.swift
+//  TrendMedia
+//
+//  Created by Donggeun Lee on 2021/10/20.
+//
+
+import UIKit
+
+class CastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
