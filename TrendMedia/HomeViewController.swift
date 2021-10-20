2500fa9db3a18951e6eed5a06069da2575d4dc56
Donggeun Lee <patlee997@gmail.com>
2021-10-20 17:21:34 +0900

    HomeViewController - Main View Controller

--- TrendMedia/HomeViewController.swift
+++ TrendMedia/HomeViewController.swift
@@ -7,23 +7,37 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
 
+    @IBOutlet weak var homeViewSmall: UIView!
+    @IBOutlet weak var homeMainLabel: UILabel!
+    @IBOutlet weak var mainButton1: UIButton!
+    @IBOutlet weak var mainButton2: UIButton!
+    @IBOutlet weak var mainButton3: UIButton!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+        
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.identifier, for: indexPath) as? HomeTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        
+        
+        return cell
+    }
+    
 }
