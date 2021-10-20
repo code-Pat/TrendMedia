ac8e18d69981577dca8a075e43a342a4d4bfb789
Donggeun Lee <patlee997@gmail.com>
2021-10-20 17:21:20 +0900

    HomeTableViewCell - for custom cell

---
+++ TrendMedia/HomeTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  HomeTableViewCell.swift
+//  TrendMedia
+//
+//  Created by Donggeun Lee on 2021/10/20.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+    
+    static let identifier = "HomeTableViewCell"
+
+    @IBOutlet weak var gerneLabel: UILabel!
+    @IBOutlet weak var titleENLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleKRLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var similarButton: UIButton!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
