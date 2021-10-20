1d0f20c1f56e6e99494efd48cba17a3f7b9666d3
Donggeun Lee <patlee997@gmail.com>
2021-10-20 19:58:51 +0900

    Media file added.

---
+++ TrendMedia/Media.swift
@@ -0,0 +1,20 @@
+//
+//  Media.swift
+//  TrendMedia
+//
+//  Created by Donggeun Lee on 2021/10/20.
+//
+
+import Foundation
+
+struct Media {
+    var title: String = ""
+    var releaseDate: String = ""
+    var genre: String = ""
+    var region: String = ""
+    var overview: String = ""
+    var rate: Double = 0.0
+    var starring: String = ""
+    var backdropImage: String = ""
+    
+}
