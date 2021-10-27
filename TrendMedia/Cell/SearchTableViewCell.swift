d14c406f7134f0a3e0be7abee6a84288a1d418a7
Donggeun Lee <patlee997@gmail.com>
2021-10-27 17:42:04 +0900

    SearchTableViewCell edited

--- TrendMedia/Cell/SearchTableViewCell.swift
+++ TrendMedia/Cell/SearchTableViewCell.swift
@@ -11,12 +11,14 @@ class SearchTableViewCell: UITableViewCell {
 
     static let identifier = "searchCell"
     
+    @IBOutlet weak var cellView: UIView!
+    
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var mediaTitleLabel: UILabel!
     @IBOutlet weak var releaseDateLabel: UILabel!
     @IBOutlet weak var summaryTextView: UITextView!
     
-    
+    /*
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -27,5 +29,12 @@ class SearchTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+     */
+    
+    func cellSetUp() {
+        self.layer.masksToBounds = false
+        self.layer.shadowOpacity = 0.8
+        self.layer.shadowOffset = CGSize(width: -2, height: -2)
+        self.layer.shadowRadius = 3
+    }
 }
