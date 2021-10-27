4ec04de7dc55ffef0844eb3ce851afea2777392b
Donggeun Lee <patlee997@gmail.com>
2021-10-27 17:42:58 +0900

    MyMediaViewController edited

--- TrendMedia/ViewController/MyMediaViewController.swift
+++ TrendMedia/ViewController/MyMediaViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class MyMediaViewController: UIViewController {
+    
+    let mediaInformation = MediaInformation()
 
     @IBOutlet weak var myMediaCollectionView: UICollectionView!
     
@@ -16,10 +18,19 @@ class MyMediaViewController: UIViewController {
 
         myMediaCollectionView.delegate = self
         myMediaCollectionView.dataSource = self
+        
+        setUpView()
     }
     
-
     
+    func setUpView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 10
+        layout.sectionInset = UIEdgeInsets(top:10, left: 10, bottom: 10, right: 10)
+        layout.estimatedItemSize = CGSize(width: (UIScreen.main.bounds.width - 40) / 2, height: (UIScreen.main.bounds.width - 40) / 2)
+        myMediaCollectionView.collectionViewLayout = layout
+    }
 }
 
 extension MyMediaViewController: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -27,12 +38,19 @@ extension MyMediaViewController: UICollectionViewDelegate, UICollectionViewDataS
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyMediaCell.identifier, for: indexPath) as? MyMediaCell else {
             return UICollectionViewCell()
         }
+        
+        let media = mediaInformation.tvShow[indexPath.row]
+        
+        cell.posterImageView.image = UIImage(named: media.title)
+        cell.titleLabel.text = media.title
+        cell.ratingLabel.text = String(media.rate)
+        
         return cell
     }
     
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return 10
     }
     
    
